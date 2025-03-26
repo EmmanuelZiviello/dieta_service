a3776baf22f7c3871554242fc47288df5641ad95
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-26 18:55:02 +0100

    Delete F_taste_dieta directory

--- F_taste_dieta/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from F_taste_dieta.models.paziente import PazienteModel
-from F_taste_dieta.models.nutrizionista import NutrizionistaModel
-from F_taste_dieta.models.alimento import AlimentoModel
-from F_taste_dieta.models.dieta import DietaModel
-from F_taste_dieta.models.pasto import PastoModel
-from F_taste_dieta.models.pietanza import PietanzaModel
--- F_taste_dieta/utils/management_utils.py
+++
@@ -1,7 +0,0 @@
-from F_taste_dieta.models.paziente import PazienteModel
-from F_taste_dieta.models.nutrizionista import NutrizionistaModel
-
-def check_nutrizionista(paziente : PazienteModel, nutrizionista : NutrizionistaModel):
-    if paziente.id_nutrizionista == nutrizionista.id_nutrizionista:
-        return True
-    return False
